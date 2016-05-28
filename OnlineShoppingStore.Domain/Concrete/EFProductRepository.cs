4f0200beb37f86f56e7df46bd6ca387fa8a31ba6
AfifaM <afifa.moosa@hotmail.com>
2016-05-28 12:43:20 +0100

    Online shopping store code

---
+++ OnlineShoppingStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private readonly EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products;
+            }
+            
+        }
+
+        public Product DeleteProduct(int productID)
+        {
+            Product dbEntry = context.Products.Find(productID);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
+
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products.Find(product.ProductID);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+}
